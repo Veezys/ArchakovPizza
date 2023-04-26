5667723868d7b523c4bd278b1e0401fb63fb70f0
imfirstplayerinyourmind <vovanchekov2@gmail.com>
2023-04-26 21:07:58 +0300

    feature: made stucture

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="wrapper">
+        <Header/>
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+             <Categories/>
+             <Sort/>
+            </div>
+            <h2 class="content__title">Все пиццы</h2>
+            <div class="content__items">
+              <PizzaBlock title="Мексиканская" price="399"/>
+              <PizzaBlock/>
+              <PizzaBlock/>
+              <PizzaBlock/>
+
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
